2c4b49051a9fc74775042bf40ebbc276f51368c5
masonville17 <anoniboni9876@gmail.com>
2025-12-12 13:34:03 -0500

    just a little cleanuyp before tackling kbs

--- Migrate-ITPortal-Hudu.ps1
+++ Migrate-ITPortal-Hudu.ps1
@@ -1,11 +1,34 @@
 $project_workdir = $PSScriptRoot
-$jobs = @("Assets-and-Layouts")
+$jobs = @(
+"read-datas",
+"Assets-and-Layouts"
+)
+
+$exportLocation = $exportLocation ?? (Read-Host "please enter the full path to your export.")
+$hudubaseUrl    = $hudubaseUrl    ?? (Read-Host "please enter the hudubase url.")
+$huduapikey     = $huduapikey     ?? (Read-Host "please enter your hudu api key.")
+$internalCompanyName = $internalCompanyName ?? (Read-Host "please enter the internal company name to use for assets without a company.")
+
+if (-not (Test-Path -Path $exportLocation)) {
+    Write-Error "The specified path does not exist: $exportLocation"; exit;
+} else {
+    write-host "Using export location: $exportLocation"
+}
+
 
 $ITPortalData =  @{}
+$MigrationErrors = @()
+
 foreach ($f in $(Get-ChildItem "$project_workdir\helpers" -Filter *.ps1)) {. $f.FullName}
 Get-PSVersionCompatible; Get-HuduModule; Set-HuduInstance; Get-HuduVersionCompatible;
 
 foreach ($job in $jobs){
     write-host "starting $job"
-    . "$project_workdir\jobs\$job.ps1"
+    try {
+        . "$project_workdir\jobs\$job.ps1"
+    } catch {
+        write-error "error during $job: $_"
+        $migrationerrors += @{job=$job; error = $_;}
+    }
+    write-host "finished $job"
 }
--- jobs/Assets-and-Layouts.ps1
+++ jobs/Assets-and-Layouts.ps1
@@ -1,37 +1,6 @@
-$exportLocation = $exportLocation ?? (Read-Host "please enter the full path to your export.")
-$hudubaseUrl    = $hudubaseUrl    ?? (Read-Host "please enter the hudubase url.")
-$huduapikey     = $huduapikey     ?? (Read-Host "please enter your hudu api key.")
-$internalCompanyName = $internalCompanyName ?? (Read-Host "please enter the internal company name to use for assets without a company.")
 
-if (-not (Test-Path -Path $exportLocation)) {
-    Write-Error "The specified path does not exist: $exportLocation"; exit;
-} else {
-    write-host "Using export location: $exportLocation"
-}
-$ITPortalData = $ITPortalData ?? @{}
 
-foreach ($f in Get-ChildItem -Path $exportLocation -Recurse -File -Filter "*.txt") {
-    Write-Host "reading $($f.FullName)"
-    $contents = Get-Content $f.FullName -Raw
-    $ITPortalData["$($f.BaseName)"] = $ITPortalData["$($f.BaseName)"] ??  @{CsvData = @(); TxtData = @()}
-    $ITPortalData["$($f.BaseName)"].TxtData+=@{ filename = $f.FullName; content = $contents; name = $f.BaseName}
-}
-
-# Import & populate ITPortalData
-foreach ($f in Get-ChildItem -Path $exportLocation -Recurse -File -Filter "*.csv") {
-    Write-Host "reading $($f.FullName)"
-
-    $contents = Import-Csv $f.FullName
-
-    $key = $f.BaseName
-
-    $ITPortalData[$key] = @{
-        Filename   = $f.FullName
-        CsvData    = @($contents)
-        Properties = (Get-CSVPropertiesSafe $contents)  # assuming this returns header names
-    }
-}
-$huducompanies = get-huducompanies
+$ITPortalData = @{}
 $companyMap = @{}
 $assetsMap = @{}
 $specialObjectTypes = @{
@@ -72,8 +41,6 @@ $articlesPropMap = @{
 
 $discernment = 4096
 $position = 0
-
-
 
 foreach ($key in $ITPortalData.Keys) {
 
