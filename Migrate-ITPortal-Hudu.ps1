96450327e0fba1f9cc079808ead9bacf2617cc78
masonville17 <anoniboni9876@gmail.com>
2026-01-14 13:30:14 -0500

    doing a full run against a tester right now, just a few things to move around - particularly ensuring that internal company is present needs to be lower down in the main script

--- Migrate-ITPortal-Hudu.ps1
+++ Migrate-ITPortal-Hudu.ps1
@@ -17,7 +17,6 @@ $jobs = @(
 $exportLocation = $exportLocation ?? (Read-Host "please enter the full path to your export.")
 $hudubaseUrl    = $hudubaseUrl    ?? (Read-Host "please enter the hudubase url.")
 $huduapikey     = $huduapikey     ?? (Read-Host "please enter your hudu api key.")
-$internalCompany = Get-OrSetInternalCompany -internalCompanyName $internalCompanyName
 
 if (-not (Test-Path -Path $exportLocation)) {
     Write-Error "The specified path does not exist: $exportLocation"; exit;
@@ -32,6 +31,8 @@ $MigrationErrors = @()
 foreach ($f in $(Get-ChildItem "$project_workdir\helpers" -Filter *.ps1)) {. $f.FullName}
  Get-PSVersionCompatible; Get-HuduModule; Set-HuduInstance; 
  Get-EnsuredPath $debugDir | Out-Null
+ $internalCompany = Get-OrSetInternalCompany -internalCompanyName $internalCompanyName
+
  foreach ($job in $jobs){
      write-host "starting $job" -foregroundColor cyan
      . "$project_workdir\jobs\$job.ps1"
